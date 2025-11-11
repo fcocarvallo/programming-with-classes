60aad6c8610d5d9521b0ac917497ae6c93121214
fracarvallo <fcocarvallo@byui.edu>
2025-11-10 22:47:09 -0700

    Files loaded and classes updated

--- sandbox/SolarSystem/Program.cs
+++ sandbox/SolarSystem/Program.cs
@@ -2,6 +2,15 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Bonjour tout le monde!");
+        Planet mercury = new Planet();
+        mercury._name = "Mercury";
+        mercury._diameter = 1234.343;
+        mercury.DisplayPlanetInformation();
+
+        Planet venus = new Planet();
+        venus._name = "Venus";
+        venus._diameter = 12353.234;
+        venus.DisplayPlanetInformation();
     }
+    
 }
