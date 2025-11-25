50968ef535c3d0a0a3e753b993990251ec3e51b7
fracarvallo <fcocarvallo@byui.edu>
2025-11-24 21:13:04 -0700

    Program finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Circle("Yellow", 7));
+        shapes.Add(new Rectangle("Red", 5, 2));
+        shapes.Add(new Square("Blue", 4));
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+            Console.WriteLine($"{shape}\nColor: {color}, Area: {area}");
+            Console.WriteLine();
+            // Console.WriteLine(shape.GetColor());
+            // Console.WriteLine(shape.GetArea());
+        
     }
+}
+
 }
---
+++ prepare/Learning05/circle.cs
@@ -0,0 +1,16 @@
+public class Circle : Shape
+{
+private double _radius;
+
+public Circle(string color, double radius) : base(color)
+{
+    _radius = radius;
+
+}
+
+public override double GetArea()
+{
+    return System.Math.PI * _radius * _radius;
+}
+
+}
